e2a0ab197eebc72bc890a94bc378f884e3db91a0
Simone Amorim <simoneas03@gmail.com>
2023-03-31 09:54:47 +0200

    refactor(chapter-3): Add while factorial

--- src/chapter-3/code/factorial.js
+++ src/chapter-3/code/factorial.js
@@ -6,4 +6,21 @@ const factorial = number => {
   }
 }
 
-factorial(10)
+factorial(3)
+
+const whileFactorial = number => {
+  let result = number
+
+  if (number === 0 || number === 1) {
+    return 1
+  }
+
+  while (number > 1) {
+    number--
+    result = result * number
+  }
+
+  return result
+}
+
+whileFactorial(4)
